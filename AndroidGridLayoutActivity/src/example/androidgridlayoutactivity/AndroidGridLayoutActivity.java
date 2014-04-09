71e2b74eb8ccbe7c36221a9e36d100bb25120c97
lwen2027 <lwen2027@gmail.com>
2014-04-08 23:33:28 -0500

    implement example db

--- AndroidGridLayoutActivity/src/example/androidgridlayoutactivity/AndroidGridLayoutActivity.java
+++ AndroidGridLayoutActivity/src/example/androidgridlayoutactivity/AndroidGridLayoutActivity.java
@@ -1,21 +1,34 @@
 package example.androidgridlayoutactivity;
 
-import android.app.Activity;
+import java.util.List;
+import java.util.Random;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.GridView;
+import android.widget.TextView;
+
+import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
+import com.j256.ormlite.dao.RuntimeExceptionDao;
  
 
-public class AndroidGridLayoutActivity extends Activity {
-
+public class AndroidGridLayoutActivity extends OrmLiteBaseActivity<DatabaseHelper> {
+	public final String TAG = "AndroidGridLayoutActivity";
+//	private Context context;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_android_grid_layout);
-
-		 GridView gridView = (GridView) findViewById(R.id.gridView1);
+		Log.d(TAG, "create grid view");
+		
+		GridView gridView = (GridView) findViewById(R.id.gridView1);
 	        // Instance of ImageAdapter Class
-	     gridView.setAdapter(new ImageAdapter(this));
+	    gridView.setAdapter(new ImageAdapter(this));
+	    
+	    TextView tv = new TextView(this);
+		doSampleDatabaseStuff("onCreate", tv);
+		setContentView(tv);
 	}
 	
 	@Override
@@ -23,6 +36,55 @@ public class AndroidGridLayoutActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.android_grid_layout, menu);
 		return true;
+	}
+	
+	private void doSampleDatabaseStuff(String action, TextView tv){
+		RuntimeExceptionDao<SimpleData, Integer> simpleDao = getHelper().getSimpleDataDao();
+		// query for all of the data objects in the database
+		List<SimpleData> list = simpleDao.queryForAll();
+		// our string builder for building the content-view
+		StringBuilder sb = new StringBuilder();
+		sb.append("got ").append(list.size()).append(" entries in ").append(action).append("\n");
+
+		// if we already have items in the database
+		int simpleC = 0;
+		for (SimpleData simple : list) {
+			sb.append("------------------------------------------\n");
+			sb.append("[").append(simpleC).append("] = ").append(simple).append("\n");
+			simpleC++;
+		}
+		sb.append("------------------------------------------\n");
+		for (SimpleData simple : list) {
+			simpleDao.delete(simple);
+			sb.append("deleted id ").append(simple.id).append("\n");
+			Log.d(TAG, "deleting simple(" + simple.id + ")");
+			simpleC++;
+		}
+
+		int createNum;
+		do {
+			createNum = new Random().nextInt(3) + 1;
+		} while (createNum == list.size());
+		for (int i = 0; i < createNum; i++) {
+			// create a new simple object
+			long millis = System.currentTimeMillis();
+			SimpleData simple = new SimpleData(millis);
+			// store it in the database
+			simpleDao.create(simple);
+			Log.d(TAG, "created simple(" + millis + ")");
+			// output it
+			sb.append("------------------------------------------\n");
+			sb.append("created new entry #").append(i + 1).append(":\n");
+			sb.append(simple).append("\n");
+			try {
+				Thread.sleep(5);
+			} catch (InterruptedException e) {
+				// ignore
+			}
+		}
+
+		tv.setText(sb.toString());
+		Log.d(TAG, "Done with page at " + System.currentTimeMillis());
 	}
 	
 	
--- AndroidGridLayoutActivity/src/example/androidgridlayoutactivity/ImageAdapter.java
+++ AndroidGridLayoutActivity/src/example/androidgridlayoutactivity/ImageAdapter.java
@@ -1,11 +1,9 @@
 package example.androidgridlayoutactivity;
 import java.io.File;
-import com.j256.ormlite.android.*;
 
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Environment;
 import android.util.Log;
 import android.view.View;
@@ -13,12 +11,32 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.media.ExifInterface;
 
 public class ImageAdapter extends BaseAdapter{
-	
+
 	private Context mContext;
-	public Uri[] bits;
-	
+	public Bitmap[] bits;
+		
+
+
+	public static int calculateInSampleSize(BitmapFactory.Options options, int reqHeight, int reqWidth){
+
+		final int height = options.outHeight;
+		final int width = options.outWidth;
+		int inSampleSize = 1;
+		
+		if (height>reqHeight || width > reqWidth){
+			final int halfHeight = height/2;
+			final int halfWidth = width/2;
+			
+			while ((halfHeight/inSampleSize)>reqHeight && (halfWidth/inSampleSize)>reqWidth){
+				inSampleSize*=2;
+			}
+		}
+		
+		return inSampleSize;
+	}
 	
 	
 	public ImageAdapter(Context c){
@@ -26,39 +44,60 @@ public class ImageAdapter extends BaseAdapter{
 		File directory = new File(sdCard.getAbsolutePath() + "/ClothesMatchingApplication");
 		File[] names = directory.listFiles();
 		Log.d("Files", "Size: "+ names.length);
-		bits = new Uri[names.length];
-		
+		bits = new Bitmap[names.length];
+
 	    mContext = c;
+	    
 	    for (int i=0; i< names.length; i++){
-            Uri uri = Uri.parse(names[i].getAbsolutePath());
-            bits[i]=uri;
+	    
+	    	Bitmap bitmap = decodeBitmap(names[i].getAbsolutePath(),250,250);
+//	    	ExifInterface exif = new ExifInterface(bitmap);
+	//    	int rotation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+	 //   	int rotationInDegrees = exifToDegrees(rotation);
+	    	
+	    	bits[i]=bitmap;
         }
 	}
-	 
+/**	
+	private static int exifToDegrees(int exifOrientation) {        
+	    if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) { return 90; } 
+	    else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {  return 180; } 
+	    else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {  return 270; }            
+	    return 0;    
+	 }
+	**/
+	public static Bitmap decodeBitmap(String names, int reqWidth, int reqHeight) {
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inJustDecodeBounds = true;
+		BitmapFactory.decodeFile(names,options);
+		options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+		options.inJustDecodeBounds = false;
+		return BitmapFactory.decodeFile(names, options);
+	}
+	
 	public int getCount() {
 	    return bits.length;
 	}
 	@Override
-	
+
 	public Object getItem(int position){
 	    return bits[position];
 	}
-	
+
 	@Override
 	public long getItemId(int position){
 
-	    return 0;
+	    return position;
 	}
-	 
+
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent){
-		
+
 	    ImageView imageView = new ImageView(mContext);
-	    imageView.setImageURI(bits[position]);
+	    imageView.setImageBitmap(bits[position]);
 	    imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-	    imageView.setLayoutParams(new GridView.LayoutParams(100, 100));
+	    imageView.setLayoutParams(new GridView.LayoutParams(250, 250));
 	    return imageView;
 	}
-	 
-}
 
+}
