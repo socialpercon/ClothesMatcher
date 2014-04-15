ed5561b030f74fff9f24e21cda091144d6646391
Tony Ren <lzr1@rice.edu>
2014-04-15 14:59:28 -0500

    Deleted last picture from camera method --> was not working as intended

--- Prototype/src/rice/clothesmatchingapplication/NewEntryHome.java
+++ Prototype/src/rice/clothesmatchingapplication/NewEntryHome.java
@@ -25,25 +25,11 @@ public class NewEntryHome extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_entry_home);
-		String[] projection = new String[]{
-			    MediaStore.Images.ImageColumns._ID,
-			    MediaStore.Images.ImageColumns.DATA,
-			    MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME,
-			    MediaStore.Images.ImageColumns.DATE_TAKEN,
-			    MediaStore.Images.ImageColumns.MIME_TYPE
-			    };
-			final Cursor cursor = getContentResolver()
-			        .query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection, null, 
-			               null, MediaStore.Images.ImageColumns.DATE_TAKEN + " DESC");
-			
-			if (cursor.moveToFirst()) {
-			    String imageLocation = cursor.getString(1);
-			    File imageFile = new File(imageLocation);
+	/**		    File imageFile = new File(imageLocation);
 			    if (imageFile.exists()) {
 			        loadIntoImageview(imageLocation);
-			    }
 			} 
-			
+		**/	
 			//loadItemIntoDatabase(name, type);
 	
 	}
