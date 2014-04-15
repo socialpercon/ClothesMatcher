0b1ef4db809681d142acda2bb8c20900408fed32
Tony Ren <lzr1@rice.edu>
2014-04-15 12:50:54 -0500

    Integrated Database into Clothes Matching Application

---
+++ Prototype/src/rice/clothesmatchingapplication/DatabaseHelper.java
@@ -0,0 +1,77 @@
+package rice.clothesmatchingapplication;
+	
+import java.sql.SQLException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+
+	public class DatabaseHelper extends OrmLiteSqliteOpenHelper{
+		private static final String DATABASE_NAME = "ClothesMatchingDb.db";
+		private static final int DATABASE_VERSION = 1;
+		private Dao<SimpleData, Integer> simpleDao = null;
+		public static DatabaseHelper helper = null;
+		private static final AtomicInteger usageCounter = new AtomicInteger(0);
+		
+		private DatabaseHelper(Context context){
+			super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		}
+		
+		public static synchronized DatabaseHelper getHelper(Context context){
+			if(helper == null){
+				helper = new DatabaseHelper(context);
+			}
+			usageCounter.incrementAndGet();
+			return helper;
+		}
+
+		@Override
+		public void onCreate(SQLiteDatabase db, ConnectionSource connectionSource) {
+			try{
+				Log.i(DatabaseHelper.class.getName(), "onCreate");
+				TableUtils.createTable(connectionSource, SimpleData.class);
+			} 
+			catch (SQLException e) {
+				Log.e(DatabaseHelper.class.getName(), "Can't create database", e);
+				throw new RuntimeException(e);
+		}
+		}
+			
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, ConnectionSource connectionSource, int oldVer,
+				int newVer) {
+			try{
+				TableUtils.dropTable(connectionSource, SimpleData.class, true);
+				onCreate(db, connectionSource);
+			}
+			catch(SQLException e){
+			throw new RuntimeException(e);
+			}
+			
+		}
+		
+		public Dao<SimpleData, Integer> getSimpleDataDao() throws SQLException {
+			if (simpleDao == null) {
+				simpleDao = getDao(SimpleData.class);
+			}
+			return simpleDao;
+		}
+		
+		public void close() {
+			if (usageCounter.decrementAndGet() == 0) {
+				super.close();
+				simpleDao = null;
+				helper = null;
+			}
+		}
+		
+	}
+
--- Prototype/src/rice/clothesmatchingapplication/OldEntryHome.java
+++ Prototype/src/rice/clothesmatchingapplication/OldEntryHome.java
@@ -1,13 +1,37 @@
 package rice.clothesmatchingapplication;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.j256.ormlite.dao.Dao;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class OldEntryHome extends Activity {
+
+public static OldEntryHome instance;
+	
+	public OldEntryHome() {
+		instance = this;
+	}
+	
+	public static Context getContext(){
+		return instance;
+	}
+	
+	private final String LOG_TAG = getClass().getSimpleName();
+	private DatabaseHelper databaseHelper = null;
+	public final String TAG = "AndroidGridLayoutActivity";
+	public ImageAdapter imageAdapter; 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +43,13 @@ public class OldEntryHome extends Activity {
 		
 		ImageView imageView = (ImageView) findViewById(R.id.imageView1);
 		imageView.setImageBitmap(bitmap);
+		
+		try {
+			imageAdapter = new ImageAdapter(instance);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -28,4 +57,88 @@ public class OldEntryHome extends Activity {
 		return true;
 	}
 
+	@Override
+	protected void onDestroy(){
+		super.onDestroy();
+		if(databaseHelper!=null){
+			databaseHelper.close();
+			databaseHelper = null;
+		}
+	}
+	
+	private DatabaseHelper getHelper(){
+		if (databaseHelper == null){
+			databaseHelper = DatabaseHelper.getHelper(this);
+		}
+		return databaseHelper;
+	}
+
+	private void doSampleDatabaseStuff(String action, TextView tv) {
+		try {
+			// our string builder for building the content-view
+			StringBuilder sb = new StringBuilder();
+			doSimpleDatabaseStuff(action, sb);
+			sb.append("------------------------------------------\n");
+			tv.setText(sb.toString());
+				
+			Log.i(LOG_TAG, "Done with page at " + System.currentTimeMillis());
+		} catch (SQLException e) {
+			Log.e(LOG_TAG, "Database exception", e);
+			tv.setText("Database exeption: " + e);
+			return;
+		}
+	}
+	
+	private StringBuilder doSimpleDatabaseStuff(String action, StringBuilder sb) throws SQLException, java.sql.SQLException {
+		// get our dao
+		Dao<SimpleData, Integer> simpleDao = getHelper().getSimpleDataDao();
+		// query for all of the data objects in the database
+		List<SimpleData> list = simpleDao.queryForAll();
+		sb.append("got ").append(list.size()).append(" SimpleData entries in ").append(action).append("\n");
+		sb.append("------------------------------------------\n");
+
+		String[] fileNames = imageAdapter.getFilePaths();
+		String name = "";
+		Boolean nameStatus = false;
+		int i=1;
+		for (String s: fileNames)
+		{
+			name = s;
+			
+			for (SimpleData data: list){
+				SimpleData Entry = data;
+				
+				if(Entry.fileName.equals(name)){
+					nameStatus = true;
+				}
+			
+			}
+			Log.d("nameStatus", "" + nameStatus);
+			//maybe the createifnotexist function would have been easier but oh well
+		if (nameStatus==false)
+		{
+		SimpleData simple = new SimpleData(name,"Pants" ,"Blue");
+		simpleDao.create(simple);
+		sb.append("------------------------------------------\n");
+		sb.append("created SimpleData entry #").append(i).append(":\n");
+		sb.append(simple).append("\n");
+		i++;
+		}
+		
+		nameStatus =false;
+		}
+//		for deleting database items, use deleteById!		
+//		for (SimpleData data: list){
+//			simpleDao.deleteById(data.id);
+//		}
+		
+		try {
+				Thread.sleep(5);
+			} catch (InterruptedException e) {
+				// ignore
+		}
+		return sb;
+	}
 }
+
+
