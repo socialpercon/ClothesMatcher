a5d1e5938a3771acb4d088913f48b7f8bbc4bd6a
Tony Ren <lzr1@rice.edu>
2014-04-15 10:33:29 -0500

    Deleted placeholder database code

--- Prototype/src/rice/clothesmatchingapplication/MainActivity.java
+++ Prototype/src/rice/clothesmatchingapplication/MainActivity.java
@@ -2,8 +2,6 @@ package rice.clothesmatchingapplication;
 
 import java.io.File;
 import java.util.Date;
-
-import rice.clothesmatchingapplication.MainActivity.FeedReaderContract.FeedEntry;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
@@ -39,31 +37,5 @@ public class MainActivity extends Activity {
 		startActivity(move);
 	}
 	
-	
-	public final class FeedReaderContract {
-	    // To prevent someone from accidentally instantiating the contract class,
-	    // give it an empty constructor.
-	public FeedReaderContract() {}
-
-	    /* Inner class that defines the table contents */
-	abstract class FeedEntry implements BaseColumns {
-	        public static final String TABLE_NAME = "entry";
-	        public static final String COLUMN_NAME_ENTRY_ID = "entryid";
-	        public static final String COLUMN_NAME_TITLE = "title";
-	        public static final String COLUMN_NAME_SUBTITLE = "subtitle";
-	    }
-	}
-	
-	private static final String TEXT_TYPE = " TEXT";
-	private static final String COMMA_SEP = ",";
-	private static final String SQL_CREATE_ENTRIES =
-	    "CREATE TABLE " + FeedEntry.TABLE_NAME + " (" +
-	    FeedEntry._ID + " INTEGER PRIMARY KEY," +
-	    FeedEntry.COLUMN_NAME_ENTRY_ID + TEXT_TYPE + COMMA_SEP +
-	    FeedEntry.COLUMN_NAME_TITLE + TEXT_TYPE + COMMA_SEP+")";
-
-	private static final String SQL_DELETE_ENTRIES =
-	    "DROP TABLE IF EXISTS " + FeedEntry.TABLE_NAME;
-}
-	
+}	
 
