0da26853106dc077618d9c11064632a7aa11e80d
Tony Ren <lzr1@rice.edu>
2014-04-16 13:51:36 -0500

    Added ImageAdapter to add only filepaths that are returned by the database query

--- Prototype/src/rice/clothesmatchingapplication/LongSleeveShirtsHome.java
+++ Prototype/src/rice/clothesmatchingapplication/LongSleeveShirtsHome.java
@@ -9,23 +9,32 @@ import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.stmt.PreparedQuery;
 import com.j256.ormlite.stmt.QueryBuilder;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 public class LongSleeveShirtsHome extends Activity {
 
 	private DatabaseHelper databaseHelper = null;
+	List<SimpleData> dataList = checkDatabaseType();
+	ArrayList<String> filePathList = new ArrayList<String>(dataList.size());
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_long_sleeve_shirts_home);
 		
-		List<SimpleData> dataList = checkDatabaseType();
-		ArrayList<String> filePathList = new ArrayList<String>(dataList.size());
 		for (SimpleData data: dataList){
 			String filePath = data.fileName;
 			Log.d("filePath", filePath);
@@ -80,5 +89,97 @@ public List<SimpleData> checkDatabaseType(){
 	return null;
 	
 }
+
+private class ImageAdapterPartial extends BaseAdapter{
+
+	private Context mContext;
+	public Bitmap[] bits;
+	
+	public int calculateInSampleSize(BitmapFactory.Options options, int reqHeight, int reqWidth){
+
+		final int height = options.outHeight;
+		final int width = options.outWidth;
+		int inSampleSize = 1;
+		
+		if (height>reqHeight || width > reqWidth){
+			final int halfHeight = height/2;
+			final int halfWidth = width/2;
+			
+			while ((halfHeight/inSampleSize)>reqHeight && (halfWidth/inSampleSize)>reqWidth){
+				inSampleSize*=2;
+			}
+		}
+		
+		return inSampleSize;
+	}
+	
+	
+	public ImageAdapterPartial(Context c) throws IOException{
+		
+		mContext = c;
+		
+	    for (int i=0; i< filePathList.size(); i++){    	
+	    	Bitmap bitmap = decodeBitmap(filePathList.get(i),250,250);
+	    	int pictureRotation = getPictureRotation(filePathList.get(i));
+	    	Matrix matrix = new Matrix();
+	    	matrix.postRotate(pictureRotation);
+	    	bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+	    	bits[i]=bitmap;
+        }
+	}
+	
+	
+	private int exifToDegrees(int exifOrientation) {        
+	    if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) { return 90; } 
+	    else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {  return 180; } 
+	    else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {  return 270; }            
+	    return 0;    
+	 }
+	
+	public int getPictureRotation(String path) throws IOException{
+		ExifInterface exif = new ExifInterface(path);
+    	int rotation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+    	int rotationInDegrees = exifToDegrees(rotation);
+    	return rotationInDegrees;
+	}
+	
+	public Bitmap decodeBitmap(String names, int reqWidth, int reqHeight) {
+		
+		
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inJustDecodeBounds = true;
+		BitmapFactory.decodeFile(names,options);
+		options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+		options.inJustDecodeBounds = false;
+		return BitmapFactory.decodeFile(names, options);
+	}
+	
+	public int getCount() {
+	    return bits.length;
+	}
+	@Override
+
+	public Object getItem(int position){
+	    return bits[position];
+	}
+
+	@Override
+	public long getItemId(int position){
+
+	    return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent){
+
+	    ImageView imageView = new ImageView(mContext);
+	    imageView.setImageBitmap(bits[position]);
+	    imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+	    imageView.setLayoutParams(new GridView.LayoutParams(250, 250));
+	    return imageView;
+	}
+
+}
+
 }
 
