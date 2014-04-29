ff11babc567e0a098b74a23af767c7cb56c16685
Salvatore Testa <sal.testa@gmail.com>
2014-04-29 15:37:37 -0500

    Update the ImageAdapter to use Picasso and keep the edit view from crashing

--- Prototype/src/rice/clothesmatchingapplication/ImageAdapter.java
+++ Prototype/src/rice/clothesmatchingapplication/ImageAdapter.java
@@ -2,6 +2,8 @@ package rice.clothesmatchingapplication;
 import java.io.File;
 import java.io.IOException;
 
+import com.squareup.picasso.Picasso;
+
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -19,8 +21,8 @@ import android.media.ExifInterface;
 public class ImageAdapter extends BaseAdapter{
 
 	private Context mContext;
-	//public Bitmap[] bits;
 	public String[] filePaths;
+	public static final String TAG = "ImageAdapter";
 	
 	public static int calculateInSampleSize(BitmapFactory.Options options, int reqHeight, int reqWidth){
 
@@ -52,19 +54,8 @@ public class ImageAdapter extends BaseAdapter{
 	    mContext = c;
 	    
 	    for (int i=0; i< names.length; i++){
-//	    	try {
 	    	filePaths[i] = names[i].getAbsolutePath();
 	    	Log.d("FilePath", names[i].getAbsolutePath());
-//	    	Bitmap bitmap = decodeBitmap(names[i].getAbsolutePath(),250,250);
-//	    	int pictureRotation;
-//				pictureRotation = getPictureRotation(names[i].getAbsolutePath());
-//	    	Matrix matrix = new Matrix();
-//	    	matrix.postRotate(pictureRotation);
-//	    	bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
-//	    	bits[i]=bitmap;
-//			} catch (IOException e) {
-//				e.printStackTrace();
-//			}
         }
 	}
 	//need to add method to return position
@@ -99,13 +90,11 @@ public class ImageAdapter extends BaseAdapter{
 	
 	public int getCount() {
 	    return filePaths.length;
-//	    return bits.length;
 	}
 	@Override
 
 	public Object getItem(int position){
 	    return filePaths[position];
-//	    return bits[position];
 	}
 
 	@Override
@@ -116,13 +105,22 @@ public class ImageAdapter extends BaseAdapter{
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent){
+		ImageView imageView = (ImageView) convertView;
+		if (imageView == null) {
+		  imageView = new ImageView(mContext);
+		  Log.d(TAG, "Initializing the image view.");
+		}
+		String url = filePaths[position];
 
-	    ImageView imageView = new ImageView(mContext);
-	    imageView.setImageURI(Uri.fromFile(new File(filePaths[position])));
-//	    imageView.setImageBitmap(bits[position]);
-	    imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-	    imageView.setLayoutParams(new GridView.LayoutParams(250, 250));
+		Log.d(TAG, url);
+		Picasso.with(mContext)
+			.load(new File(url))
+			.centerCrop()
+			.resize(250, 250)
+			.into(imageView);
+
 	    return imageView;
+	    
 	}
 
 }
